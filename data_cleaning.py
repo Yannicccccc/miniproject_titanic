a1fb76dc5306ef99548ae9abc3318b1c7b37a1bb
Yannicccccc <41508764+Yannicccccc@users.noreply.github.com>
2020-11-13 17:14:13 +0800

    Add files via upload

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,15 +3,11 @@ import torch
 from sklearn.model_selection import train_test_split
 
 
-data = pd.read_csv('data/titanic/train.csv', header=0)
-TOTAL = len(data)
-
-
-def get_cabin():
+def get_cabin(dataset):
     cabin_list = []
-    for i in range(TOTAL):
-        if not pd.isnull(data['Cabin'][i]):
-            list = data['Cabin'][i].split()
+    for i in range(len(dataset)):
+        if not pd.isnull(dataset['Cabin'][i]):
+            list = dataset['Cabin'][i].split()
             for j in range(len(list)):
                 if list[j] not in cabin_list:
                     cabin_list.append(list[j])
@@ -19,41 +15,58 @@ def get_cabin():
     return sorted(cabin_list)
 
 
-cabin_list = get_cabin()
+
 # dropping unecessary parameters
-def helper(i):
-    ans = [0] * 10
-    ans[0] = data['PassengerId'][i]
-    ans[1] = data['Pclass'][i]
-    ans[4] = data['SibSp'][i]
-    ans[5] = data['Parch'][i]
-    ans[6] = data['Fare'][i]
+def helper(dataset, i, cabin_list, mode):
+    ans = [-1] * 10 if mode== 'train' else [-1] * 9
 
-    if data['Sex'][i] == 'male': ans[2] = 1
-    else: ans[2] = 2
+    ans[0] = dataset['PassengerId'][i]
+    ans[1] = dataset['Pclass'][i]
+    ans[2] = 1 if dataset['Sex'][i] == 'male' else 2
 
-    if not pd.isnull(data['Age'][i]): ans[3] = data['Age'][i]
+    if not pd.isnull(dataset['Age'][i]): ans[3] = dataset['Age'][i]
 
-    if not pd.isnull(data['Cabin'][i]):
-        list = data['Cabin'][i].split()
+    ans[4] = dataset['SibSp'][i]
+    ans[5] = dataset['Parch'][i]
+
+    if not pd.isnull(dataset['Fare'][i]): ans[6] = dataset['Fare'][i]
+
+    if not pd.isnull(dataset['Cabin'][i]):
+        list = dataset['Cabin'][i].split()
         ans[7] = torch.mean(torch.FloatTensor([cabin_list.index(list[j]) for j in range(len(list))]))
 
-    if data['Embarked'][i] == 'C': ans[8] = 1
-    elif data['Embarked'][i] == 'Q': ans[8] = 2
-    elif data['Embarked'][i] == 'S': ans[8] = 3
+    if dataset['Embarked'][i] == 'C': ans[8] = 1
+    elif dataset['Embarked'][i] == 'Q': ans[8] = 2
+    elif dataset['Embarked'][i] == 'S': ans[8] = 3
 
-    ans[9] = data['Survived'][i]
+    if mode=='train': ans[9] = dataset['Survived'][i]
 
     return ans
 
 
-def load_data():
-    trX = torch.Tensor([helper(i)[0:8] for i in range(TOTAL)])
-    trY = torch.Tensor([helper(i)[9] for i in range(TOTAL)])
-    # print(trX, trY)
+def load_data(dataset):
+    if dataset == 'train':
+        data = pd.read_csv("data/titanic/train.csv", header=0)
+        TOTAL = len(data)
+        cabin_list = get_cabin(data)
+        trX = torch.Tensor([helper(data, i, cabin_list, 'train')[0:9] for i in range(TOTAL)])
+        trY = torch.LongTensor([helper(data, i, cabin_list, 'train')[9] for i in range(TOTAL)])
+        # print(trX, trY)
+        # print(trX.shape)
 
-    # Random Splitting
-    train_vectors, test_vectors, train_labels, test_labels = train_test_split(trX, trY, test_size=0.15)
-    return(train_vectors, test_vectors, train_labels, test_labels)
+        # Random Splitting
+        train_vectors, test_vectors, train_labels, test_labels = train_test_split(trX, trY, test_size=0.15)
+        return (train_vectors, test_vectors, train_labels, test_labels)
 
-# load_data()
+    elif dataset == 'test':
+        data = pd.read_csv("data/titanic/test.csv", header=0)
+        TOTAL = len(data)
+        cabin_list = get_cabin(data)
+        data_ = data
+        test = torch.Tensor([helper(data_, i, cabin_list, 'test') for i in range(TOTAL)])
+        # print(test)
+        # print(test.shape)
+        return test
+
+# load_data('train')
+# load_data('test')
--- data_visualization.py
+++ data_visualization.py
@@ -3,72 +3,160 @@ matplotlib.use('TkAgg')
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from data_cleaning import helper, get_cabin
+import warnings
 
 
+warnings.filterwarnings('ignore')
 sns.set(style="white", color_codes=True)
-data = pd.read_csv("data/titanic/train.csv", header=0)
-TOTAL = len(data)
 
-data['Survived'] = data['Survived'].map({
-    0: 'Died',
-    1: 'Survived'
-})
 
-data['Embarked'] = data['Embarked'].map({
-    'C':'Cherbourg',
-    'Q':'Queenstown',
-    'S':'Southampton',
-})
+def plotting(data):
+    data['Survived'] = data['Survived'].map({
+        0: 'Died',
+        1: 'Survived'
+    })
 
-ax = sns.countplot(x = 'Pclass', hue = 'Survived', palette = 'hls', data = data)
-ax.set(title = 'Passenger status (Survived/Died) against Passenger Class', xlabel = 'Passenger Class', ylabel = 'Total')
-# plt.show()
+    data['Embarked'] = data['Embarked'].map({
+        'C': 'Cherbourg',
+        'Q': 'Queenstown',
+        'S': 'Southampton',
+    })
 
-ax = sns.countplot(x = 'Sex', hue = 'Survived', palette = 'hls', data = data)
-ax.set(title = 'Total Survivors According to Sex', xlabel = 'Sex', ylabel='Total')
-# plt.show()
+    ax = sns.countplot(x = 'Pclass', hue = 'Survived', palette = 'hls', data = data)
+    ax.set(title = 'Passenger status (Survived/Died) against Passenger Class', xlabel = 'Passenger Class', ylabel = 'Total')
+    plt.show()
 
-interval = (0, 15, 30, 45, 60, 75, 90)
-categories = ['0~15','15~30','30~45', '45~60', '60~75','75~90']
-data['Age_cats'] = pd.cut(data.Age, interval, labels = categories)
-ax = sns.countplot(x = 'Age_cats',  data = data, hue = 'Survived', palette = 'hls')
-ax.set(xlabel='Age Categorical', ylabel='Total', title="Age Categorical Survival Distribution")
-# plt.show()
+    ax = sns.countplot(x = 'Sex', hue = 'Survived', palette = 'hls', data = data)
+    ax.set(title = 'Total Survivors According to Sex', xlabel = 'Sex', ylabel='Total')
+    plt.show()
 
-ax = sns.countplot(x = 'SibSp', hue = 'Survived', palette = 'hls', data = data)
-ax.set(title = 'Survival distribution according to Number of siblings/spouses on board')
-# plt.show()
+    interval = (0, 15, 30, 45, 60, 75, 90)
+    categories = ['0~15','15~30','30~45', '45~60', '60~75','75~90']
+    data['Age_cats'] = pd.cut(data.Age, interval, labels = categories)
+    ax = sns.countplot(x = 'Age_cats',  data = data, hue = 'Survived', palette = 'hls')
+    ax.set(xlabel='Age Categorical', ylabel='Total', title="Age Categorical Survival Distribution")
+    plt.show()
 
-ax = sns.countplot(x = 'Parch', hue = 'Survived', palette = 'hls', data = data)
-ax.set(title = 'Survival distribution according to number of parents/children on board')
-# plt.show()
+    ax = sns.countplot(x = 'SibSp', hue = 'Survived', palette = 'hls', data = data)
+    ax.set(title = 'Survival distribution according to Number of siblings/spouses on board')
+    plt.show()
 
-interval = (0, 15, 30, 45, 100, 300, 550)
-categories = ['0~15','15~30','30~45', '45~100', '100~300','300~550']
-data['Fare_cats'] = pd.cut(data.Fare, interval, labels = categories)
-ax = sns.countplot(x = 'Fare_cats', data = data, hue = 'Survived', palette = 'hls')
-ax.set(title = 'Survival distribution according to Passenger fare')
-# plt.show()
+    ax = sns.countplot(x = 'Parch', hue = 'Survived', palette = 'hls', data = data)
+    ax.set(title = 'Survival distribution according to number of parents/children on board')
+    plt.show()
 
-df = data['Cabin'].dropna()
-data['Cabin_cats'] = df.astype(str).str[0]
-ax = sns.countplot(x = 'Cabin_cats', data = data, hue = 'Survived', palette = 'hls')
-ax.set(title = 'Survival distribution according to Cabin no')
-# plt.show()
+    interval = (0, 15, 30, 45, 100, 300, 550)
+    categories = ['0~15','15~30','30~45', '45~100', '100~300','300~550']
+    data['Fare_cats'] = pd.cut(data.Fare, interval, labels = categories)
+    ax = sns.countplot(x = 'Fare_cats', data = data, hue = 'Survived', palette = 'hls')
+    ax.set(title = 'Survival distribution according to Passenger fare')
+    plt.show()
 
-ax = sns.countplot(x = 'Embarked', hue = 'Survived', palette = 'hls', data = data)
-ax.set(title = 'Survival distribution according to Embarking place')
-# plt.show()
+    df = data['Cabin']
+    data['Cabin_cats'] = df.astype(str).str[0]
+    ax = sns.countplot(x = 'Cabin_cats', data = data, hue = 'Survived', palette = 'hls')
+    ax.set(title = 'Survival distribution according to Cabin no')
+    plt.show()
 
-data_ = data.drop(['Name', 'Ticket', 'Cabin', 'PassengerId', 'Age_cats', 'Fare_cats'], 1)
-# print(data_)
-data_.Sex.replace(('male','female'), (0,1), inplace = True)
-data_.Embarked.replace(('Southampton','Cherbourg','Queenstown'), (0,1,2), inplace = True)
-data_.Survived.replace(('Died','Survived'), (0,1), inplace = True)
-data_.Cabin_cats.replace(('A', 'B', 'C', 'D', 'E', 'F', 'G', 'T'), (1,2,3,4,5,6,7,8), inplace = True)
-data_.Cabin_cats.fillna(0, inplace=True)
-#print(data_.head())
+    ax = sns.countplot(x = 'Embarked', hue = 'Survived', palette = 'hls', data = data)
+    ax.set(title = 'Survival distribution according to Embarking place')
+    plt.show()
 
-plt.figure(figsize=(14,12))
-sns.heatmap(data_.astype(float).corr(),linewidths=0.1, square=True,  linecolor='white', annot=True)
-plt.show()
+    data_ = data.drop(['Name', 'Ticket', 'Cabin', 'PassengerId', 'Age_cats', 'Fare_cats'], 1)
+    data_.Sex.replace(('male','female'), (0,1), inplace = True)
+    data_.Embarked.replace(('Southampton','Cherbourg','Queenstown'), (0,1,2), inplace = True)
+    data_.Survived.replace(('Died','Survived'), (0,1), inplace = True)
+    data_.Cabin_cats.replace(('A', 'B', 'C', 'D', 'E', 'F', 'G', 'T', 'n'), (1,2,3,4,5,6,7,8,-1), inplace = True)
+    data_.Cabin_cats.fillna(0, inplace=True)
+    # print(data_.head())
+
+    plt.figure(figsize=(14,12))
+    sns.heatmap(data_.astype(float).corr(),linewidths=0.1, square=True,  linecolor='white', annot=True)
+    plt.show()
+
+
+def data_features():
+    data = pd.read_csv("data/titanic/train.csv", header=0)
+    TOTAL = len(data)
+
+    cabin_list = get_cabin(data)
+    data__ = pd.DataFrame([helper(data, i, cabin_list, 'train') for i in range(TOTAL)],
+                         columns=["PassengerID", "Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Cabin", "Embarked", "Survived"])
+    data__.drop(['PassengerID', 'Survived'], 1, inplace=True)
+    train_feature = pd.DataFrame(data={'NumSam': [], 'Mean': [], 'Variance': []})
+    train_feature.NumSam = data__[data__ != -1].count()
+    train_feature.Mean = data__.mean()
+    train_feature.Variance = data__.var()
+    print(train_feature)
+    train_feature.to_csv("data/data_visualization/train_data_feature.csv")
+
+    data = pd.read_csv("data/titanic/test.csv", header=0)
+    TOTAL = len(data)
+
+
+    cabin_list = get_cabin(data)
+    data__ = pd.DataFrame([helper(data, i, cabin_list, 'test') for i in range(TOTAL)],
+                          columns=["PassengerID", "Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Cabin", "Embarked"])
+    data__.drop(['PassengerID'], 1, inplace=True)
+    train_feature = pd.DataFrame(data={'NumSam': [], 'Mean': [], 'Variance': []})
+    train_feature.NumSam = data__[data__ != -1].count()
+    train_feature.Mean = data__.mean()
+    train_feature.Variance = data__.var()
+    print(train_feature)
+    train_feature.to_csv("data/data_visualization/test_data_feature.csv")
+
+    cabin_list = get_cabin(data)
+    data__ = pd.DataFrame([helper(data, i, cabin_list, 'test') for i in range(TOTAL)],
+                          columns=["PassengerID", "Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Cabin",
+                                   "Embarked"])
+    data__.drop(['PassengerID'], 1, inplace=True)
+    train_feature = pd.DataFrame(data={'NumSam': [], 'Mean': [], 'Variance': []})
+    train_feature.NumSam = data__[data__ != -1].count()
+    train_feature.Mean = data__.mean()
+    train_feature.Variance = data__.var()
+    print(train_feature)
+    train_feature.to_csv("data/data_visualization/test_data_feature.csv")
+
+
+
+def prediction_eval():
+    df_prediction = pd.read_csv('data/titanic/prediction.csv')
+    df_prediction = df_prediction[df_prediction.Survived == 1]
+    print(len(df_prediction), 'survivors classified')
+    print(len(df_prediction[df_prediction.Sex == 'female']), 'survived females passengers')
+    print(len(df_prediction[df_prediction.Age < 18]), 'survived passengers under 18')
+    print(len(df_prediction[df_prediction.SibSp == 0][df_prediction.Parch == 0]),
+          'survived passengers do not have any family member on board')
+
+    temp = min(len(df_prediction[df_prediction.Pclass == 1]),
+               len(df_prediction[df_prediction.Pclass == 2]),
+               len(df_prediction[df_prediction.Pclass == 3]))
+
+    str = 'have the least chance of surviving the tragedy'
+    if len(df_prediction[df_prediction.Pclass == 1]) == temp: print('Ticket class 1', str)
+    elif len(df_prediction[df_prediction.Pclass == 2]) == temp: print('Ticket class 2', str)
+    else: print('Ticket class 3', str)
+
+    temp = min(len(df_prediction[df_prediction.Embarked == 'C']),
+               len(df_prediction[df_prediction.Embarked == 'Q']),
+               len(df_prediction[df_prediction.Embarked == 'S']))
+
+    if len(df_prediction[df_prediction.Embarked == 'C']) == temp: print('Cherbourg', str)
+    elif len(df_prediction[df_prediction.Embarked == 'Q']) == temp: print('Queenstown', str)
+    else: print('Southampton', str)
+
+
+
+def result_comparison():
+    df_ = pd.read_csv('data/titanic/result_comparison.csv', header=0)
+    list_ = ['survivor', 'female', 'eighteen', 'family', 'class']
+    for i in range(len(list_)):
+        print(df_[list_[i]].mean())
+        print(df_[list_[i]].var())
+
+
+# data_features()
+# plotting(pd.read_csv("data/titanic/train.csv", header=0))
+# prediction_eval()
+# result_comparison()
